491f9cea0a774c725e562278f89471f7c2fc7116
Brahith <brahithp@gmail.com>
2024-11-21 00:24:13 -0500

    restarted my code cause too many bugs were showing up and everything was in confusion.

--- TestAverage/config/app.js
+++ TestAverage/config/app.js
@@ -1,53 +1,53 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const createError = require('http-errors');
-const mongoose = require('mongoose');
-const DB = require('./db');
-const testRouter = require('../routes/testMark'); // Ensure this path is valid
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-const app = express();
+let indexRouter = require('../routes/index');
+let usersRouter = require('../routes/users');
+let bookRouter = require('../routes/book')
 
-// Mongoose connection setup
-mongoose.connect(DB.URI, {
-  useNewUrlParser: true, // Correct typo
-  useUnifiedTopology: true,
-});
-
-const mongoDB = mongoose.connection;
-mongoDB.on('error', console.error.bind(console, 'Connection Error'));
-mongoDB.once('open', () => {
-  console.log('MongoDB Connected');
-});
-
-// View engine setup
-app.set('views', path.join(__dirname, 'views')); // Corrected path
+let app = express();
+let mongoose = require('mongoose');
+let DB = require('./db');
+// point my mongoose to the URI
+mongoose.connect(DB.URI);
+let mongoDB = mongoose.connection;
+mongoDB.on('error',console.error.bind(console,'Connection Error'))
+mongoDB.once('open',()=>{
+  console.log('MongoDB Connected')
+})
+mongoose.connect(DB.URI,{useNewURIParser:true,
+  useUnifiedTopology:true
+})
+// view engine setup
+app.set('../views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../node_modules')));
-app.use('/', testRouter); // Root route setup
-
-// Static files
-app.use('/Content', express.static(path.join(__dirname, '../public/Content')));
-
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/books',bookRouter);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Error handler
-app.use(function (err, req, res, next) {
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
-  res.render('error', { title: 'Error' });
+  res.render('error',{title:'Error'});
 });
 
 module.exports = app;
--- TestAverage/config/db.js
+++ TestAverage/config/db.js
@@ -1,4 +1,1 @@
-module.exports = {
-    URI: process.env.MONGO_URI || "mongodb+srv://brahithpavanantharajah:brahith2005@cluster0.vl86n.mongodb.net/dataList",
-  };
-    
+module.exports = {"URI":"mongodb+srv://brahithpavanantharajah:brahith2005@cluster0.vl86n.mongodb.net/dataList"}
